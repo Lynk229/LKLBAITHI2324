bd839b903aba9508751747ad34d4e5ec9c57d047
Lynk229 <nguyenly10072002@gmail.com>
2024-06-17 13:48:30 +0700

    add file

---
+++ Models/Person.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Net.Http.Headers;
+
+namespace LKLBAITHI2324.Models
+{
+    public class Person
+    {
+        public string Ten SV { get; set; }
+        
+        public string Ngay sinh { get; set; }
+
+        public string MSV { get; set; }
+
+    }
+}
